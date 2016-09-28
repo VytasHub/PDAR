45e5303508051d2fc606169ae9d3a783ea8cf293
jimduggan <jim.duggan@nuigalway.ie>
2016-09-28 21:15:34 +0100

    Updating files from lecture 3

--- R code/04 Matrices & DF/03 Spreadsheet.R
+++ R code/04 Matrices & DF/03 Spreadsheet.R
@@ -1,15 +1,14 @@
 
 library(gdata) 
+library(xlsx)
 
 pop <- read.xls("R code/04 Matrices & DF/CensusData.xlsx")
 
 
 pop$Total <- pop$Leinster+pop$Munster+pop$Connacht+pop$Ulster
 
+write.xlsx(x = pop, file = "./R code/04 Matrices & DF/Output.xlsx",
+           sheetName = "Processesed Data", row.names = FALSE)
 
-
-qplot(faithful$eruptions,faithful$waiting)+
-  xlab("Eruption Time")+
-  ylab("Waiting time until next Eruption")
 
 
