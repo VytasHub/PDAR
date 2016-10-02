8a11d1ba1d1228db54891d9ca474cf365cf679d5
jimduggan <jim.duggan@nuigalway.ie>
2016-10-02 16:47:12 +0100

    Updates for lecture 5...

--- R code/04 Matrices & DF/03 Spreadsheet.R
+++ R code/04 Matrices & DF/03 Spreadsheet.R
@@ -7,8 +7,11 @@ pop <- read.xls("R code/04 Matrices & DF/CensusData.xlsx")
 
 pop$Total <- pop$Leinster+pop$Munster+pop$Connacht+pop$Ulster
 
+library(xlsx)
+
 write.xlsx(x = pop, file = "./R code/04 Matrices & DF/Output.xlsx",
            sheetName = "Processesed Data", row.names = FALSE)
+
 
 
 
---
+++ R code/05 S3 Objects/04 Inheritance.R
@@ -0,0 +1,49 @@
+
+
+f<-function(x){
+  UseMethod("f")
+}
+
+g <-function(x){
+  UseMethod("g")  
+}
+
+f.a<-function(x){
+  print("function f.a")
+}
+
+f.b<-function(x){
+  print("function f.b")
+}
+
+g.a<-function(x){
+  print("function g.a")
+}
+
+
+
+z<-structure(list(),class=c("b","a"))
+
+class(z)
+
+f(z)
+
+g(z)
+
+
+p<-structure(list(),class=c("c","data.frame"))
+
+methods(class="data.frame")
+
+
+
+
+d<-structure(list(),class=c("df1","data.frame"))
+
+print.df1<-function(x){
+  print(Sys.time())
+  print(data.frame(x))
+}
+
+
+
